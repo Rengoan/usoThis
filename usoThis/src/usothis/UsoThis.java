a7dd79f2f98323d6f9c4707528123649937d2e76
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-09-29 11:08:56 +0200

    usoThis

--- usoThis/src/usothis/UsoThis.java
+++ usoThis/src/usothis/UsoThis.java
@@ -16,9 +16,19 @@ class Persona {
 
     //Constructor
     public Persona(String nombre, String apellidos) {
+        //Se utiliza this en un contecto dinamico dentro de una clase.
         this.nombre = nombre;
         this.apellidos = apellidos;
-        System.out.println("Acceso a persona con uso de this" + this);
+        System.out.println("Acceso a persona con uso de this " + this);
+        new Imprimir().imprimir(this);
     }
 
+}
+
+class Imprimir {
+
+    public void imprimir(Persona persona) {
+        System.out.println("Imprimimos persona desde el metodo imprimir: " + persona);
+        System.out.println("Imprimimos persona haciendo uso de this desde imprimir " + this);
+    }
 }
